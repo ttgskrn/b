b716eecb0dd6d1a8f6490fe271fbc9f132e360be
Tetege Eskaeren <ttgskrn@gmail.com>
2016-10-22 03:20:29 +0800

    tocbylabel.js

---
+++ tocbylabel.js
@@ -0,0 +1,25 @@
+var maxposts = 100;
+
+function createtocbylabel(posts) {
+
+  for (var i = 0; i < maxposts; i++) {
+
+    var post = posts.feed.entry[i];
+    var posttitle = post.title.$t;
+    var posturl;
+
+    if (i == posts.feed.entry.length) break;
+
+    for (var j = 0; j < post.link.length; j++) {
+
+      if (post.link[j].rel == 'alternate') {
+        posturl = post.link[j].href;
+        break;
+      }
+    }
+
+    posttitle = posttitle.link(posturl);
+
+    document.write('<li>' + posttitle + '</li>');
+  }
+}
